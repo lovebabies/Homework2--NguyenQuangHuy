ed42009d0ba783271fcf9eb64186cc48096dae7d
jayden nguyen <jaydennguyen1211@gmail.com>
2017-07-26 16:14:23 +0700

    session 2 first commit

---
+++ turtle2.py
@@ -0,0 +1,10 @@
+from turtle import *
+shape('turtle')
+for n in range(3,7):
+    if n % 2 == 0:
+        color('red')
+    else:
+        color('blue')
+    for i in range(n):
+        forward(100)
+        left(360/n)
